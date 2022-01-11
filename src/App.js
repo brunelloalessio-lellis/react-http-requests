0aaf70abfb44cbc7ff2dfa5bd685051d7e183902
abrunello <brunello@bludata.com>
2022-01-11 14:29:08 +0100

    using states to hand loading state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMoviesHandler = async () => {
+
+    setIsLoading(true)
+
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
     const transformedMovies = data.results.map((movieData) => {
@@ -19,6 +23,7 @@ function App() {
     });
 
     setMovies(transformedMovies);
+    setIsLoading(false)
   };
 
   return (
@@ -27,7 +32,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading ...</p>}
       </section>
     </React.Fragment>
   );
